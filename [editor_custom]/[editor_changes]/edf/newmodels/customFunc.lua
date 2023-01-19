27159fa5ac892cc04beceea001a2f96a3227f7ed
Fernando-A-Rocha <frocha2001@gmail.com>
2023-01-19 22:41:13 +0000

    Improve & editor custom funcs & fix create element base ID

--- [editor_custom]/[editor_changes]/edf/newmodels/customFunc.lua
+++ [editor_custom]/[editor_changes]/edf/newmodels/customFunc.lua
@@ -13,10 +13,12 @@ newmodelsKey['ped'] = exports.newmodels:getDataNameFromType('ped')
 
 function isCustomModID(elementType, modelid)
 	local isCustom, mod, customElementType = exports.newmodels:isCustomModID(modelid)
-	if isCustomModID and customElementType == elementType then
-		return true
-	elseif isCustomModID and customElementType == "player" and elementType == "ped" then
-		return true
+	if isCustomModID and
+		(customElementType == elementType)
+		or ((customElementType == "player" and elementType == "ped") or (customElementType == "ped" and elementType == "player"))
+		or ((customElementType == "pickup" and elementType == "object") or (customElementType == "object" and elementType == "pickup"))
+	then
+		return true, mod
 	else
 		return false
 	end
@@ -32,24 +34,19 @@ end
 function setElementModel(element, modelid)
 	if isElement(element) then
 		local elementType = getElementType(element)
-		if elementType == 'vehicle' then
+		if elementType == 'vehicle' or elementType == 'ped' or elementType == 'object' then
 			if exports.newmodels:isDefaultID(elementType, modelid) then
 				_setElementModel(element, modelid)
-			elseif isCustomModID(elementType, modelid) then
-				setElementData(element, newmodelsKey[elementType], modelid)
+			else
+				local isCustom, mod = isCustomModID(elementType, modelid)
+				if isCustom and mod then
+					setElementData(element, newmodelsKey[elementType], modelid)
+				else
+					outputDebugString('Invalid modelid: '..tostring(modelid)..' on '..elementType, 2)
+				end
 			end
-		elseif elementType == 'ped' then
-			if exports.newmodels:isDefaultID(elementType, modelid) then
-				_setElementModel(element, modelid)
-			elseif isCustomModID(elementType, modelid) then
-				setElementData(element, newmodelsKey[elementType], modelid)
-			end
-		elseif elementType == 'object' then
-			if exports.newmodels:isDefaultID(elementType, modelid) then
-				_setElementModel(element, modelid)
-			elseif isCustomModID(elementType, modelid) then
-				setElementData(element, newmodelsKey[elementType], modelid)
-			end
+		else
+			_setElementModel(element, modelid)
 		end
 	end
 end
@@ -57,35 +54,44 @@ end
 function createObject(modelid, x, y, z, rot, synced)
 	if exports.newmodels:isDefaultID('object', modelid) then
 		return _createObject(modelid, x, y, z, rot, synced)
-	elseif isCustomModID('object', modelid) then
-		local object = _createObject(1343, x, y, z, rot, synced)
-		setElementData(object, newmodelsKey['object'], modelid, not isClient)
-		return object
 	else
-		return _createObject(1343, x, y, z, rot, synced)
+		local isCustom, mod = isCustomModID('object', modelid)
+		if isCustom and mod  then
+			local object = _createObject(mod.base_id or 1343, x, y, z, rot, synced)
+			setElementData(object, newmodelsKey['object'], modelid, not isClient)
+			return object
+		else
+			return _createObject(1343, x, y, z, rot, synced)
+		end
 	end
 end
 
 function createPed(modelid, x, y, z, rot, synced)
 	if exports.newmodels:isDefaultID('ped', modelid) then
 		return _createPed(modelid, x, y, z, rot, synced)
-	elseif isCustomModID('ped', modelid) then
-		local ped = _createPed(0, x, y, z, rot, synced)
-		setElementData(ped, newmodelsKey['ped'], modelid, not isClient)
-		return ped
 	else
-		return _createPed(0, x, y, z, rot, synced)
+		local isCustom, mod = isCustomModID('ped', modelid)
+		if isCustom and mod  then
+			local ped = _createPed(mod.base_id or 1, x, y, z, rot, synced)
+			setElementData(ped, newmodelsKey['ped'], modelid, not isClient)
+			return ped
+		else
+			return _createPed(0, x, y, z, rot, synced)
+		end
 	end
 end
 
 function createVehicle(modelid, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
-	if exports.newmodels:isDefaultID('vehicle', modelid) then
-		return _createVehicle(modelid, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
-	elseif isCustomModID('vehicle', modelid) then
-		local veh = _createVehicle(400, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
-		setElementData(veh, newmodelsKey['vehicle'], modelid, not isClient)
-		return veh
-	else
-		return _createVehicle(400, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
+    if exports.newmodels:isDefaultID('vehicle', modelid) then
+        return _createVehicle(modelid, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
+    else
+		local isCustom, mod = isCustomModID('vehicle', modelid)
+		if isCustom and mod then
+			local veh = _createVehicle(mod.base_id or 400, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
+			setElementData(veh, newmodelsKey['vehicle'], modelid, not isClient)
+			return veh
+		else
+			return _createVehicle(400, x, y, z, rx, ry, rz, numberplate, bDirection, variant1, variant2)
+		end
 	end
 end
