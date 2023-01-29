697266769fed61b8e39ed6511a51227693bade08
Fernando-A-Rocha <frocha2001@gmail.com>
2023-01-29 17:48:49 +0000

    Update mod_list.lua

--- newmodels/mod_list.lua
+++ newmodels/mod_list.lua
@@ -11,17 +11,21 @@
 ]]
 
 -- ....................................... modList explained .......................................
+	
+-- The following mod list is stored in server only, then sent to client on request
+-- These are the default mods, more than be added via functions from other resources
 
 -- 'id' must be unique and out of the default GTA (& preferrably SA-MP too) ID ranges
 
 -- 'base_id' is the model the mod will inherit some properties from
--- Doesn't make much difference on peds(skins), but it does on vehicles & objects
+--	Doesn't make much difference on peds(skins), but it does on vehicles & objects
 
 -- 'path' can be:
 --		a string, in which case it expects files to be named ID.dff or ID.txd in that folder
 -- 		an array(table), in which case it expects an array of file names like {dff="filepath.dff", txd="filepath.txd", col="filepath.col"}
+--  		For files encrypted using NandoCrypt, don't add the .nandocrypt extension, it's defined by the 'NANDOCRYPT_EXT' setting
 -- 	All paths defined manually in this file need to be local (this resource)
--- 	To add a mod from another resource see the examples provided in the documentation.
+--		To add a mod from another resource see the examples provided in the documentation.
 
 -- 'name' can be whatever you want (string)
 
@@ -35,10 +39,6 @@
 
 modList = {
 	
-	-- stored in server only, then sent to client on request
-	-- these are the default mods, more than be added via functions
-	
-	-- element type
 	ped = {
 
 		{id=20001, base_id=1, path="models/peds/", name="Mafioso 1"},
@@ -52,9 +52,6 @@ modList = {
 		{id=80003, base_id=400, path="models/vehicles/", name="86 Landstalker 1"},
 		{id=80004, base_id=400, path="models/vehicles/", name="98 Landstalker 1"},
 		{id=80005, base_id=468, path="models/vehicles/", name="Sanchez Test", ignoreTXD=true},
-
-		-- Models encrypted using NandoCrypt
-		-- Don't add the .nandocrypt extension, it's defined by the 'NANDOCRYPT_EXT' setting
 		{id=80006, base_id=507, path={dff="models/vehicles/elegant.dff", txd="models/vehicles/elegant.txd"}, name="Elegant Test"},
 	},
 
