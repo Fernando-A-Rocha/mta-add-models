Fernando-A-Rocha/mta-add-models

commit b83099236eac41cc8e4985f072e53bc6cf0386fa

add getCustomModelFromElement

newmodels_reborn/scripts/core/client_logic.lua
@@ -8,7 +8,7 @@ local currFreeIdDelay = 9500   -- ms
 local FREE_ID_DELAY_STEP = 500 -- ms
 
 local function applyElementCustomModel(element)
-    local customModel = tonumber(getElementData(element, getCustomModelDataKey(element)))
+    local customModel = getCustomModelFromElement(element)
     if not customModel then return end
     local loadedModel = loadedModels[customModel]
     if not loadedModel then return end
@@ -142,8 +142,8 @@ end
 local function isCustomModelInUse(customModel, loadedModel)
     if loadedModel then
         for _, elementType in pairs(loadedModel.elementTypes) do
-            for _, v in pairs(getElementsByType(elementType, root, true)) do
-                if tonumber(getElementData(v, getCustomModelDataKey(elementType))) == customModel then
+            for _, element in pairs(getElementsByType(elementType, root, true)) do
+                if getCustomModelFromElement(element) == customModel then
                     return true
                 end
             end
@@ -213,7 +213,7 @@ local function freeAllocatedModelIfUnused(customModel)
 end
 
 local function setElementCustomModel(element)
-    local customModel = tonumber(getElementData(element, getCustomModelDataKey(element)))
+    local customModel = getCustomModelFromElement(element)
     if not customModel then return end
     if not loadedModels[customModel] then
         loadCustomModel(customModel, element)
@@ -224,7 +224,7 @@ end
 
 addEventHandler("onClientElementDataChange", root, function(key, prevCustomModel, newCustomModel)
     if not isValidElement(source) then return end
-    if key ~= getCustomModelDataKey(source) then return end
+    if key ~= getCustomModelFromElement(source) then return end
     prevCustomModel = tonumber(prevCustomModel)
 
     -- Get the base model of the previous custom model the element has
@@ -255,14 +255,14 @@ end)
 
 addEventHandler("onClientElementStreamOut", root, function()
     if not isValidElement(source) then return end
-    local customModel = tonumber(getElementData(source, getCustomModelDataKey(source)))
+    local customModel = getCustomModelFromElement(source)
     if not customModel then return end
     freeAllocatedModelIfUnused(customModel)
 end)
 
 addEventHandler("onClientElementDestroy", root, function()
     if not isValidElement(source) then return end
-    local customModel = tonumber(getElementData(source, getCustomModelDataKey(source)))
+    local customModel = getCustomModelFromElement(source)
     if not customModel then return end
     freeAllocatedModelIfUnused(customModel)
 end)

newmodels_reborn/scripts/core/shared_exported.lua
@@ -20,6 +20,10 @@ setPickupTypeMTA = setPickupType
 getElementModelMTA = getElementModel
 setElementModelMTA = setElementModel
 
+function getCustomModelFromElement(element)
+    return tonumber(getElementData(element, getCustomModelDataKey(element)))
+end
+
 function getSharedCustomModelsTable()
     if IS_IMPORTED then
         return exports["newmodels_reborn"]:getCustomModels()
@@ -215,14 +219,14 @@ end
 function getElementModel(element)
     assert(isElement(element), "Invalid element passed: " .. tostring(element))
     assert(isValidElement(element), "Invalid element type passed: " .. getElementType(element))
-    return tonumber(getElementData(element, getCustomModelDataKey(element))) or getElementModelMTA(element)
+    return getCustomModelFromElement(element) or getElementModelMTA(element)
 end
 
 function getElementBaseModel(element)
     if not isClientsideScript then
         return getElementModelMTA(element)
     end
-    local customModel = tonumber(getElementData(element, getCustomModelDataKey(element)))
+    local customModel = getCustomModelFromElement(element)
     if not customModel then
         return getElementModelMTA(element)
     end

newmodels_reborn/scripts/optional/debug/c_debug.lua
@@ -43,7 +43,7 @@ local function pairsByKeys(t)
 end
 
 local function getElementCustomModelString(element)
-    local customModel = tonumber(getElementData(element, getCustomModelDataKey(element)))
+    local customModel = getCustomModelFromElement(element)
     if customModel and customModels[customModel] then
         local name, baseModel = customModels[customModel].name, customModels[customModel].baseModel
         return {("%d \"%s\" (%d)"):format(customModel, name, baseModel), 0xffffa263}

newmodels_reborn/scripts/optional/debug/s_debug.lua
@@ -64,7 +64,7 @@ addCommandHandler("testspawn", function(thePlayer)
     local dataName = getCustomModelDataKey("player")
     local x,y,z = getElementPosition(thePlayer)
     local _, _, rz = getElementRotation(thePlayer)
-    local customModel = tonumber(getElementData(thePlayer, dataName))
+    local customModel = getCustomModelFromElement(thePlayer)
     spawnPlayer(thePlayer, x,y,z, rz, getElementModelMTA(thePlayer), getElementInterior(thePlayer), getElementDimension(thePlayer))
     setElementData(thePlayer, dataName, nil)
     if customModel then

commit 9d4ac461bcd97d29a1f35df35b923640aa641a05

fix bug in onClientElementDataChange introduced by b83099236eac41cc8e4985f072e53bc6cf0386fa

newmodels_reborn/scripts/core/client_logic.lua
@@ -139,7 +139,8 @@ local function loadCustomModel(customModel, elementToApply)
     end
 end
 
-local function isCustomModelInUse(customModel, loadedModel)
+local function isCustomModelInUse(customModel)
+    local loadedModel = loadedModels[customModel]
     if loadedModel then
         for _, elementType in pairs(loadedModel.elementTypes) do
             for _, element in pairs(getElementsByType(elementType, root, true)) do
@@ -187,29 +188,24 @@ local function freeAllocatedModelNow(customModel)
     loadedModels[customModel] = nil
 end
 
-local function freeAllocatedModel(customModel, loadedModel, onlyIfUnused)
+local function freeAllocatedModel(customModel)
+    local loadedModel = loadedModels[customModel]
+    if not loadedModel then return end
     if loadedModel.disableAutoFree then
         return
-    end
-
-    if isTimer(loadedModel.freeAllocatedTimer) then
-        killTimer(loadedModel.freeAllocatedTimer)
     end
     -- Do not free all models at once, delay each model by a bit
     currFreeIdDelay = currFreeIdDelay + FREE_ID_DELAY_STEP
 
+    if isTimer(loadedModel.freeAllocatedTimer) then
+        killTimer(loadedModel.freeAllocatedTimer)
+    end
     loadedModel.freeAllocatedTimer = setTimer(function()
-        if (not onlyIfUnused) or (onlyIfUnused and not isCustomModelInUse(customModel, loadedModel)) then
+        if not isCustomModelInUse(customModel) then
             freeAllocatedModelNow(customModel)
         end
         currFreeIdDelay = currFreeIdDelay - FREE_ID_DELAY_STEP
     end, currFreeIdDelay, 1)
-end
-
-local function freeAllocatedModelIfUnused(customModel)
-    local loadedModel = loadedModels[customModel]
-    if not loadedModel then return end
-    freeAllocatedModel(customModel, loadedModel, true)
 end
 
 local function setElementCustomModel(element)
@@ -224,7 +220,7 @@ end
 
 addEventHandler("onClientElementDataChange", root, function(key, prevCustomModel, newCustomModel)
     if not isValidElement(source) then return end
-    if key ~= getCustomModelFromElement(source) then return end
+    if key ~= getCustomModelDataKey(source) then return end
     prevCustomModel = tonumber(prevCustomModel)
 
     -- Get the base model of the previous custom model the element has
@@ -244,7 +240,7 @@ addEventHandler("onClientElementDataChange", root, function(key, prevCustomModel
     end
     if prevCustomModel then
         -- Free the previous custom model if it's not used by any other element
-        freeAllocatedModelIfUnused(prevCustomModel)
+        freeAllocatedModel(prevCustomModel)
     end
 end)
 
@@ -257,14 +253,14 @@ addEventHandler("onClientElementStreamOut", root, function()
     if not isValidElement(source) then return end
     local customModel = getCustomModelFromElement(source)
     if not customModel then return end
-    freeAllocatedModelIfUnused(customModel)
+    freeAllocatedModel(customModel)
 end)
 
 addEventHandler("onClientElementDestroy", root, function()
     if not isValidElement(source) then return end
     local customModel = getCustomModelFromElement(source)
     if not customModel then return end
-    freeAllocatedModelIfUnused(customModel)
+    freeAllocatedModel(customModel)
 end)
 
 local function restoreElementBaseModels()