eab9b5ca21c5cf47f4e62b23565c308fb4a6cde6
Fernando-A-Rocha <frocha2001@gmail.com>
2025-02-26 10:35:11 +0000

    Update isDefaultObjectID: add clothing comp IDs

--- newmodels_azul/scripts/core/shared_exported.lua
+++ newmodels_azul/scripts/core/shared_exported.lua
@@ -77,10 +77,17 @@ local IDS_PEDS = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
 local IDS_VEHICLES = { 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519, 520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611 }
 
 newmodelsUtils.isDefaultObjectID = function(id)
+    -- check if id is clothing component
+    --   https://wiki.multitheftauto.com/wiki/Clothing_Component_IDs
+    --   https://github.com/multitheftauto/mtasa-blue/blob/master/Client/mods/deathmatch/logic/CModelNames.h#L11
+    if id >= 30000 and id <= 30541 then
+        return false
+    end
+    -- check if outside object id range
     if id < 321 or id > 18630 then
         return false
     end
-    -- exclude unused/reserved for other purposes IDs
+    -- check if ids are unused/reserved for other purposes
     if id >= 374 and id <= 614 then
         return false
     end
@@ -96,7 +103,11 @@ end
 function isDefaultID(elementType, id)
     id = tonumber(id)
     if not id then return false end
-    if not elementType then -- check all IDs
+    if not elementType then
+        -- Check all IDs
+        if newmodelsUtils.isDefaultObjectID(id) then
+            return true
+        end
         for _, id2 in pairs(IDS_PEDS) do
             if id2 == id then
                 return true
@@ -107,21 +118,18 @@ function isDefaultID(elementType, id)
                 return true
             end
         end
-        return newmodelsUtils.isDefaultObjectID(id)
-    else
-        if elementType == "ped" or elementType == "player" then
-            for _, id2 in pairs(IDS_PEDS) do
-                if id2 == id then
-                    return true
-                end
+    elseif elementType == "ped" or elementType == "player" then
+        for _, id2 in pairs(IDS_PEDS) do
+            if id2 == id then
+                return true
             end
-        elseif elementType == "object" or elementType == "pickup" then
-            return newmodelsUtils.isDefaultObjectID(id)
-        elseif elementType == "vehicle" then
-            for _, id2 in pairs(IDS_VEHICLES) do
-                if id2 == id then
-                    return true
-                end
+        end
+    elseif elementType == "object" or elementType == "pickup" then
+        return newmodelsUtils.isDefaultObjectID(id)
+    elseif elementType == "vehicle" then
+        for _, id2 in pairs(IDS_VEHICLES) do
+            if id2 == id then
+                return true
             end
         end
     end
